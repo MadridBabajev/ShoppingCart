2b0f415d19f6927c63746627ce9e4580dce8488c
MadridBabajev <mbabajev2602@gmail.com>
2024-09-01 02:18:40 +0300

    #0002 Implement image to bytes converter

---
+++ App/WebApp/Helpers/FileOperations.cs
@@ -0,0 +1,12 @@
+﻿namespace Helpers;
+
+public static class FileOperations
+{
+    public static byte[] LoadImageAsBytes(string imagePath)
+    {
+        using var image = File.OpenRead(imagePath);
+        using var memoryStream = new MemoryStream();
+        image.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
