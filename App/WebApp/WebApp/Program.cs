1d5732c72cd9eeaa1d1a15fe17117f383d0ec7a1
MadridBabajev <mbabajev2602@gmail.com>
2024-09-01 02:16:43 +0300

    #0002 Improve application setup pipeline

--- App/WebApp/WebApp/Program.cs
+++ App/WebApp/WebApp/Program.cs
@@ -1,13 +1,81 @@
+using System.Text;
+using App.DAL;
+using App.DAL.Contracts;
+using App.DAL.Seeding;
+using App.Domain.Identity;
 using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using WebApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// ===================== Set up dependency injection for the container =====================
+// Postgres DB connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Postgres connection line
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// Register our UOW with scoped lifecycle
+builder.Services.AddScoped<IAppUOW, AppUOW>();
+
+// Configure the Identity
+builder.Services
+    .AddIdentity<AppUser, AppRole>(
+        options => options.SignIn.RequireConfirmedAccount = false)
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<UserManager<AppUser>>();
+builder.Services.AddScoped<RoleManager<AppRole>>();
+
+builder.Services
+    .AddAuthentication()
+    .AddCookie(options => { options.SlidingExpiration = true; })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = builder.Configuration.GetValue<string>("JWT:Issuer")!,
+            ValidAudience = builder.Configuration.GetValue<string>("JWT:Audience")!,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWT:Key")!)),
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Optional", policy =>
+    {
+        policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireAssertion(_ => true);
+    });
+});
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsAllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
 
 // Versioning
 builder.Services.AddApiVersioning(options =>
@@ -34,19 +102,19 @@ var app = builder.Build();
 
 // ===================== Pipeline Setup =====================
 
+// Set up the database configurations and seed the initial data
+SetupAppData(app, app.Configuration, app.Environment);
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+if (app.Environment.IsDevelopment()) app.UseMigrationsEndPoint();
+else app.UseHsts();
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseCors("CorsAllowAll");
+
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.UseSwagger();
@@ -67,4 +135,69 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// ===================== Run the server and start waiting for requests =====================
+
 app.Run();
+
+static void SetupAppData(IApplicationBuilder app, IConfiguration configuration, IWebHostEnvironment env)
+{
+    using var serviceScope = app.ApplicationServices
+        .GetRequiredService<IServiceScopeFactory>()
+        .CreateScope();
+    using var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+    // Get the DataGuids instance from the DI container
+    var guids = serviceScope.ServiceProvider.GetRequiredService<DataGuids>();
+    
+    if (context == null)
+    {
+        throw new ApplicationException("Problem in services. Can't initialize DB Context");
+    }
+
+    using UserManager<AppUser>? userManager = serviceScope.ServiceProvider.GetService<UserManager<AppUser>>();
+    using var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<AppRole>>();
+    
+    if (userManager == null || roleManager == null)
+    {
+        throw new ApplicationException("Problem in services. Can't initialize UserManager or RoleManager");
+    }
+    
+    var logger = serviceScope.ServiceProvider.GetService<ILogger<IApplicationBuilder>>();
+    if (logger == null)
+    {
+        throw new ApplicationException("Problem in services. Can't initialize logger");
+    }
+
+    if (context.Database.ProviderName!.Contains("InMemory"))
+    {
+        return;
+    }
+
+    if (configuration.GetValue<bool>("DataInit:DropDatabase"))
+    {
+        logger.LogWarning("Dropping database");
+        AppDataInit.DropDatabase(context);
+        logger.LogWarning("Clearing database data");
+        AppDataInit.ClearData(context);
+    }
+
+    if (configuration.GetValue<bool>("DataInit:MigrateDatabase"))
+    {
+        logger.LogInformation("Migrating database");
+        AppDataInit.MigrateDatabase(context);
+    }
+
+    if (configuration.GetValue<bool>("DataInit:SeedIdentity"))
+    {
+        logger.LogInformation("Seeding identity");
+        AppDataInit.SeedIdentity(userManager, context, guids);
+    }
+
+    if (configuration.GetValue<bool>("DataInit:SeedData"))
+    {
+        logger.LogInformation("Seed app data");
+        AppDataInit.SeedAppData(context, Path.Combine(env.WebRootPath, "imgs"), guids);
+    }
+}
+
+public partial class Program;
